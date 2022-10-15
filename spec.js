403f7e6892d50203261658d8bb08a09d9ee749e9
ericpkatz <ericpkatz@gmail.com>
2022-10-15 07:46:14 -0400

    commit

--- spec.js
+++ spec.js
@@ -1,6 +1,12 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+
 const { expect } = require('chai');
 
 describe('addition', ()=> {
+  beforeEach(async()=> {
+    await conn.sync({ force: true });
+  });
   describe('1 + 1', ()=> {
     it('equals 2', ()=> {
       expect(1 + 1).to.equal(2);
